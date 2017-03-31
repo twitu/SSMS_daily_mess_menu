ced4932b59e84dfe79051351bac6b2fa152936bf
Ishan-sopho <f2016075@pilani.bits-pilani.ac.in>
2017-03-31 19:03:32 +0530

    Displaying data for a specific day
    Displaying data for a specific day.
    remaining:
    beautify HTML page

--- menu/views.py
+++ menu/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from .models import Items, Meal
+from datetime import date
 
 def index(request):
-    return HttpResponse("this page wil display daily mess menu")
+    # current_date in 'yyyy-mm-dd' format
+    current_date = '2017-02-14'  # explicitly setting date for debugging
+    #current_date = date.today().isoformat()
+    meal_types = Meal.objects.filter(date=current_date)
+    day = meal_types[0].day
+    food = [Items.objects.filter(meal=i) for i in meal_types]
+    return render(request, 'menu.html', {'types':meal_types, 'items':food, 'date':current_date, 'day':day})
